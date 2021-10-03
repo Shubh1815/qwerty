117ea4e5c24f93f3aab1d5d0446a4d3cfdadda9e
Shubh1815 <shubhparmar14@gmail.com>
2021-10-03 13:04:49 +0530

    add price field to Item and rename amount field on Transaction

--- backend/qwerty/apps/core/models/transaction.py
+++ backend/qwerty/apps/core/models/transaction.py
@@ -6,19 +6,23 @@ from qwerty.apps.accounts.models import StudentUser
 
 
 class Item(models.Model):
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
     transaction = models.ForeignKey("Transaction", on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    price_per_quantity = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, editable=False
+    )
     quantity = models.PositiveIntegerField()
 
 
 class Transaction(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    student = models.ForeignKey(StudentUser, on_delete=models.CASCADE)
     products = models.ManyToManyField(
         "Product", related_name="transactions", through="Item"
     )
-    amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
-    student = models.ForeignKey(StudentUser, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
