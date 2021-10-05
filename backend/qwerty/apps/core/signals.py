80bb6d9591c4090de3a0f42d5d8996aeae1fb5b7
Shubh1815 <shubhparmar14@gmail.com>
2021-10-05 17:01:01 +0530

    add signals for copying amount and updating student balance

--- backend/qwerty/apps/core/signals.py
+++ backend/qwerty/apps/core/signals.py
@@ -1,23 +1,17 @@
-from django.core.exceptions import ValidationError
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
-from .models import Item
+from .models import Item, Transaction
 
 
 @receiver(pre_save, sender=Item)
-def copy_amount_and_update_student_balance(sender, instance, **kwargs):
-    instance.price_per_quantity = instance.product.amount
+def copy_amount(sender, instance, **kwargs):
+    if not instance.price_per_quantity:
+        instance.price_per_quantity = instance.product.amount
 
-    price = instance.price_per_quantity * instance.quantity
-    transaction = instance.transaction
-    student_data = transaction.student.student
 
-    if student_data.balance < price:
-        raise ValidationError("Student doesn't have enough balance")
-
-    transaction.total_amount += price
-    transaction.save()
-
-    student_data.balance -= price
+@receiver(post_save, sender=Transaction)
+def update_student_balance(sender, instance, **kwargs):
+    student_data = instance.student.student
+    student_data.balance -= instance.total_amount
     student_data.save()
