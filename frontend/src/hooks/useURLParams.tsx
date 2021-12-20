9b543bf136fc0743f7ebd5c54fb9d8cfb3727e1c
Shubh1815 <shubhparmar14@gmail.com>
2021-12-20 20:12:33 +0530

    add hook for getting query parameters

---
+++ frontend/src/hooks/useURLParams.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useURLParams = () => {
+    const { search } = useLocation();
+  
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
+export default useURLParams;
+
+
+  
