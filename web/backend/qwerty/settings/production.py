d432024c8094eac9b0adbf12035cbd6e2a2c4183
Shubh Parmar <59529068+Shubh1815@users.noreply.github.com>
2022-01-03 19:43:40 +0530

    Update prodcution settings

--- web/backend/qwerty/settings/production.py
+++ web/backend/qwerty/settings/production.py
@@ -1,7 +1,25 @@
 from .base import *
 
 # ==============================================================================
+# CORE SETTINGS
+# ==============================================================================
+
+DEBUG = False
+
+# ==============================================================================
 # EMAIL CONFIGURATIONS
 # ==============================================================================
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# ==============================================================================
+# Celery CONFIGURATIONS
+# ==============================================================================
+
+CELERY = {
+    'BROKER_URL': env.str('CELERY_BROKER'),
+    'CELERY_IMPORTS': ('Worker.tasks', ),
+    'CELERY_TASK_SERIALIZER': 'json',
+    'CELERY_RESULT_SERIALIZER': 'json',
+    'CELERY_ACCEPT_CONTENT': ['json'],
+}
